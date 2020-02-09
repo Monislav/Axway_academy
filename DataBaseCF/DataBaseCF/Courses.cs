330f6509db6e58f32060a8471a618a395cc6fe32
Monislav <32357169+Monislav@users.noreply.github.com>
2020-02-09 22:35:57 +0200

    Add files via upload

---
+++ DataBaseCF/DataBaseCF/Courses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseCF
+{
+    public class Courses
+    {
+
+        public int Id { get; set; }
+        public int CoursesID { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ DataBaseCF/DataBaseCF/Grade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseCF
+{
+    public enum Grades
+    {
+        Failed,
+        Fine,
+        Good,
+        VeryGood,
+        Excellent
+    }
+    public class Grade
+    {
+        public int GradeID { get; set; }
+        public int StudentID { get; set; }
+        public Grades ? Grades { get; set; }
+        public int CoursesID { get; set; }
+
+        public virtual Student Student { get; set; }
+        public virtual Courses Courses { get; set; }
+    }
+}
